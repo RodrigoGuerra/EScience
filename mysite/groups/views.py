7b8ecf6737f5dd7606e445f39a33b969a8e10981
Thiago Cordeiro <thiago.castro.c@homail.com>
2017-05-25 20:34:06 -0300

    consertos tela Groups

--- mysite/groups/views.py
+++ mysite/groups/views.py
@@ -3,6 +3,18 @@ from django.template.loader import get_template
 from service.groups import *
 
 def groups(request):
+	create_group({
+				'Owner': 'thiago',
+	              'News': 'abc',
+	              'Members  ':[ {'Name': 'rodrigo'}, {'Name': 'leonardo'}],
+	              'Description': 'escience',
+	              'SharedItems': '5',
+	              'announcements': 'trabalho',
+	              'Tags': [{'Name':'escience'},{'Name':'scripts'}],
+	              'CreatedAt': 'abc',
+	              'UniqueName': 'Grupo Bolado',
+	              'CreditedBy': 'rlt'})
+	
 	mostRecent  = get_groups_mostRecent()
 	mostMembers = get_groups_mostMembers()
 	mostShared = get_groups_sharedItems()
